aef28b698159f4d7fed186b934e5379007003169
eduardo <eduardo.saavedra@correounivalle.edu.co>
2018-04-19 19:10:17 -0500

    guardando ngmodel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title   = 'lady';
-  name    = "Eduardo";
-  email   = "eduardo.gmail.com";
-  webSite = "https://www.google.com.co/";
-  hobbies = ['run', 'read', 'write']
+  title       : string;
+  name        : string;
+  email       : string;
+  webSite     : string;
+  hobbies     : string[];
+  showHobbies : boolean;
+
+  constructor(){
+    //apenas la pagina se ejecute se ejecuta el contructor
+    this.title        = 'lady';
+    this.name         = "Eduardo";
+    this.email        = "eduardo.gmail.com";
+    this.webSite      = "https://www.google.com.co/";
+    this.hobbies      = ['run', 'read', 'write'];
+    this.showHobbies  = false; 
+  }
+
+  newHobby(hobby){
+    this.hobbies.push(hobby.value);
+    hobby.value = '';
+    return false;
+  }
+
+  toggleHobbies(){
+    this.showHobbies  = !this.showHobbies;
+  }
 }
